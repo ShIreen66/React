e23e053b76c370735b90849258861589b20f11d4
ShIreen66 <066shiree@gmail.com>
2024-12-19 19:50:26 +0530

    add

--- HOC/vite-project/src/App.jsx
+++ HOC/vite-project/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   const [data, setdata] =useState("important dataa")
   return (
     <div className="rounded w-[80%] mx-auto p-10 bg-red-400">
-      <h1 className="text-5xl">HOC</h1>
+      <h1 className="text-5xl pb-4">HOC (Higher Order Component)</h1>
     <div className="rounded w-[80%] mx-auto p-10 bg-zinc-200">
       <h1 className='text-xl'>App Component</h1>
       <Child data={data}/>
